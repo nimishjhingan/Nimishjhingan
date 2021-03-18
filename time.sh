80ffaabcd5551f0de000939e7964ad4a5fc6860d
nimishjhingan <80863578+nimishjhingan@users.noreply.github.com>
2021-03-18 17:09:58 +0530

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 time=$(date +%M) #Returns minutes
-if test $time -lt 20; then  #Checks minutes less than 20mins
-                                      #If yes then does nothing 
+if test $time -lt 20; then  #Checks minutes less than 20mins #If yes then does nothing 
+exit
 elif test $time -lt 40; then  #Checks minutes btw 20mins to 40mins 
-          echo -e "\a"             #If yes then does one chime  
- else                                  #Checks minutes btw 40 mins to 60 mins  
-          echo -e "\a"            #one chime 
-          sleep 1                   
-          echo -e "\a"      
- fi
+echo -e "\a"             #If yes then does one chime  
+else                                  #Checks minutes btw 40 mins to 60 mins  
+echo -e "\a"            #one chime 
+sleep 1                   
+echo -e "\a"      
+fi
